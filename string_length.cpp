04e420b5a5f9421694dc2b3cd0cf5808cf7c8d05
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-09-03 15:15:02 +0530

    Add files via upload

---
+++ string_length.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main ()
+{
+    char str[50];
+    int len;
+    cout << "Enter an array or string : ";
+    gets(str);
+    length = strlen(str);
+    cout << "Length of the string is : " << length;
+    return 0;
+}
