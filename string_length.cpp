7b498b868790d624cdfd80d5da59e428206cc77b
Mohit saini <66358041+Mohitsaini2028@users.noreply.github.com>
2021-09-03 15:40:27 +0530

    Update string_length.cpp

--- string_length.cpp
+++ string_length.cpp
@@ -4,10 +4,17 @@ using namespace std;
 int main ()
 {
     char str[50];
-    int len;
+    int length;
     cout << "Enter an array or string : ";
-    gets(str);
+    cin>>str;
     length = strlen(str);
     cout << "Length of the string is : " << length;
     return 0;
 }
+
+
+//Output
+/*
+Enter an array or string : 455vvbcnb
+Length of the string is : 9
+*/
